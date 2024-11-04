8460bd66e7eda25c03ce66a2cd42ed6936389da3
Gabriel Ventura (AQ3023672) <g.ventura@aluno.ifsp.edu.br>
2024-11-04 18:41:36 -0300

    Desenvolvendo um sorteador

---
+++ Aplicativo_3_Sorteador/app/src/main/java/br/edu/ifsp/dmo1/sorteador/model/Draw.kt
@@ -0,0 +1,30 @@
+package br.edu.ifsp.dmo1.sorteador.model
+
+class Draw(private val border: Int = 0) {
+
+    private lateinit var strategy: SorteioStrategy
+    private val history = HashSet<Int>()
+
+    init {
+        if(border == 0)
+            strategy = DefaultLimit
+        else
+            strategy = DefinedLimit(border)
+    }
+
+    fun getNumber(): Int{
+        var number: Int
+
+        do{
+            number = strategy.nextNumber()
+        }while (!history.add(number))
+
+        return number
+    }
+
+    fun getHistory() = ArrayList(history)
+
+    fun getLowBorder() = strategy.getLowBorder()
+
+    fun getHighBorder() = strategy.getHighBorder()
+}
---
+++ Aplicativo_3_Sorteador/app/src/main/java/br/edu/ifsp/dmo1/sorteador/view/MainActivity.kt
@@ -0,0 +1,64 @@
+package br.edu.ifsp.dmo1.sorteador.view
+
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo1.sorteador.R
+import br.edu.ifsp.dmo1.sorteador.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo1.sorteador.model.Draw
+
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private var draw = Draw()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setClickLitener()
+    }
+
+    override fun onClick(v: View) {
+        when(v){
+            binding.buttonUseLimit -> {
+                val limit: Int = try{
+                    binding.editLimit.text.toString().toInt()
+                }catch (e: NumberFormatException){
+                    -1
+                }
+                draw = if(limit > 1)
+                    Draw(limit)
+                else
+                    Draw()
+                updateUI()
+            }
+
+            binding.buttonDraw -> {
+                binding.textviewExit.text = draw.getNumber().toString()
+                updateListView()
+            }
+        }
+    }
+
+    private fun setClickLitener(){
+        binding.buttonDraw.setOnClickListener(this)
+        binding.buttonUseLimit.setOnClickListener(this)
+    }
+
+    private fun updateUI(){
+        val str = String.format("Intervalo de 1 à %,d.", draw.getHighBorder())
+        binding.textviewInterval.text = str
+        binding.editLimit.text.clear()
+        binding.textviewExit.text = getString(R.string.inicie_o_sorteio)
+        updateListView()
+    }
+
+    private fun updateListView(){
+        val adapter: ArrayAdapter<Int> = ArrayAdapter(this, android.R.layout.simple_list_item_1, draw.getHistory())
+        binding.listviewDraw.adapter = adapter
+    }
+}
